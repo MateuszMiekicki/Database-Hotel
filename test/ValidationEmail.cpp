a89e283bed45aa76784b6aa4ad08e518f07dbb42
Mateusz Miękicki <miekickimateusz@gmail.com>
2020-02-09 13:58:32 +0100

    adtional test for email unit test

--- test/ValidationEmail.cpp
+++ test/ValidationEmail.cpp
@@ -7,16 +7,38 @@ TEST(ValidationEmail, passEmail)
 {
 	EXPECT_TRUE(email.validated(std::string("ala@email.com")));
 }
+
 TEST(ValidationEmail, invalidEmailNoDot)
 {
 	EXPECT_FALSE(email.validated(std::string("ala@emailcom")));
 }
+
 TEST(ValidationEmail, invalidEmailNoAfterDot)
 {
 	EXPECT_FALSE(email.validated(std::string("ala@email.")));
 }
+
 TEST(ValidationEmail, invalidEmailNoDomenAddres)
 {
 	EXPECT_FALSE(email.validated(std::string("ala@.com")));
 }
 
+TEST(ValidationEmail, emptyEmail)
+{
+	EXPECT_FALSE(email.validated(std::string("")));
+}
+
+TEST(ValidationEmail, withSpaceBetwenAt)
+{
+	EXPECT_FALSE(email.validated(std::string("ala @email.com")));
+}
+
+TEST(ValidationEmail, withSpaceOnEnd)
+{
+	EXPECT_FALSE(email.validated(std::string("ala@email.com ")));
+}
+
+TEST(ValidationEmail, withSpaceOnBegin)
+{
+	EXPECT_FALSE(email.validated(std::string("ala@email.com ")));
+}
