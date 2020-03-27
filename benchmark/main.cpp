61406589069555027240422c97ac75f375fff93c
Mateusz Miekicki <miekickimateusz@gmail.com>
2020-03-27 16:34:27 +0100

    create cmake special for google bench

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -1,18 +1,13 @@
-#include "benchmark/include/benchmark/benchmark.h"
+#include "build/install/include/benchmark/benchmark.h"
 
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
+static void BM_SomeFunction(benchmark::State& state) {
+  // Perform setup here
+  for (auto _ : state) {
+    // This code gets timed
+
+  }
 }
 // Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
-
+BENCHMARK(BM_SomeFunction);
+// Run the benchmark
 BENCHMARK_MAIN();
